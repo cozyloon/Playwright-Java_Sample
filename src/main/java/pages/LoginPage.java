573d5401b57a8dfc23b615cc5af57735c426ade4
Chathumal Sangeeth <38062287+cozyloon@users.noreply.github.com>
2023-10-19 15:02:10 +0530

    Update LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,19 +1,33 @@
 package pages;
 
 
-import static util.DriverSetup.page;
+import com.microsoft.playwright.Page;
 
 public class LoginPage {
-    private String txtUsername = "id=user-name";
+    private Page page;
+    private String txtUsername = "#user-name";
     private String txtPassword = "Password";
-    private String btnLogin = "id=login-button";
+    private String btnLogin = "#login-button";
+
+    public LoginPage(Page page) {
+        this.page = page;
+    }
+
+    public HomePage userLogin(String username, String password) {
+        page.locator(txtUsername).type(username);
+        page.getByPlaceholder(txtPassword).type(password);
+        page.locator(btnLogin).press("Enter");
+        return new HomePage(page);
+    }
 
     public void setUsername(String username) {
         page.locator(txtUsername).type(username);
     }
+
     public void setPassword(String password) {
         page.getByPlaceholder(txtPassword).type(password);
     }
+
     public void clickLoginBtn() {
         page.locator(btnLogin).press("Enter");
     }
